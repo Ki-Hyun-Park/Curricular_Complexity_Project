ee0619dd43e3b31e4396b5700ba9097c38e0e2d0
Ki Hyun Park <74960356+Ki-Hyun-Park@users.noreply.github.com>
2020-12-09 00:05:31 +0900

    Create abbrevs.R

---
+++ abbrevs.R
@@ -0,0 +1,17 @@
+library(rvest)
+library(stringr)
+library(dplyr)
+url <- "https://www.registrar.ucla.edu/Faculty-Staff/Courses-and-Programs/Department-and-Subject-Area-Codes"
+
+code_table <- url %>%
+  read_html %>%                          # read as html 
+  html_nodes('td:nth-child(3)') %>%      # query the paragraph under h2 headings
+  html_text %>% 
+  cbind(subject = .,
+        code = 
+          url %>%
+          read_html %>%
+          html_nodes('td:nth-child(4)') %>%
+          html_text %>%
+          str_trim(side = "both")) %>%
+  write.csv(file = "abbrev_table.csv", row.names = FALSE)
